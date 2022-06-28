b75fed378c26b10659732b246810c3ae5566fc04
Erick <ericckm@outlook.com.br>
2022-06-28 11:35:45 -0300

    first commit

---
+++ src/components/atoms/HeaderTitle/index.tsx
@@ -0,0 +1,7 @@
+export const HeaderTitle = ({description}) => {
+  return (
+    <h2 className="cursor-pointer text-xl font-bold border p-4 rounded border-blue-400">
+      {description}
+    </h2>
+  )
+}
---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header"
+
+export const Homepage = () => {
+  return (
+    <div className="flex flex-col min-h-screen min-w-screen p-4 bg-gray-300">
+      <Header />
+
+      <div>
+    teste
+      </div>
+    </div>
+  )
+}
